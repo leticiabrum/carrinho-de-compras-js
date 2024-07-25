44f408041a3de104c0856ad5ca8775d167cdda90
leticiabrum <leticiabrumcorrealeite@gmail.com>
2024-07-25 15:16:55 -0300

    add-linhas-comentadas(rascunho)

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 let carrinhoCompras = [];
 let produtoSelecionado = document.getElementById('produto').value;
+let listaDeProdutos = document.getElementById('carrinho-box');
 let itemCarrinho = document.createElement('div');
 
 function adicionar() {
     let textoInicial = document.getElementById('texto-inicial-carrinho');
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let produtoSelecionado = document.getElementById('produto').value;
+    // let valorProduto = document.getElementsById('valor-produto').textContent;
 
 
     if (carrinhoCompras.length == 0) {
         carrinhoCompras.push({
             produto: produtoSelecionado,
-            unidades: quantidade
+            unidades: quantidade,
+            // valor: valorProduto,
         })
     } else {
         itensRepetidos(produtoSelecionado, quantidade);
@@ -21,28 +24,40 @@ function adicionar() {
         textoInicial.remove();
     }
 
+    // valorProduto.classList.add('texto-azul');
+
     itensCarrinho();
+
 }
+
 
 function itensCarrinho() {
     let sectionCarrinho = document.getElementById('carrinho-box');
+    // let valorProduto = document.getElementsByClassName('opcoes').textContent;
     let itemCarrinho = document.createElement('div');
     let itemProduto = document.createElement('span');
     let itemUnidade = document.createElement('span');
-
+    // let itemValor = document.createElement('span');
+    
+    
     itemUnidade.classList.add('texto-azul');
     itemCarrinho.classList.add('produto-carrinho')
     itemCarrinho.id = 'item-carrinho';
-
+    
+    
+    
     for (let i = 0; i < carrinhoCompras.length; i++) {
+        itemUnidade.id = carrinhoCompras[i].produto;
+        itemProduto.id = carrinhoCompras[i].produto;
+        // itemValor.textContent = valorProduto;
         itemProduto.textContent = carrinhoCompras[i].produto;
         itemUnidade.textContent = carrinhoCompras[i].unidades + "x ";
         itemCarrinho.appendChild(itemUnidade);
         itemCarrinho.appendChild(itemProduto);
     }
 
-    console.log('ok')
     sectionCarrinho.appendChild(itemCarrinho);
+    // console.log('esse é o valor', valorProduto);
 }
 
 
@@ -50,8 +65,13 @@ function itensRepetidos(produtoSelecionado, quantidade) {
 
     for (let i = 0; i < carrinhoCompras.length; i++) {
         if (carrinhoCompras.length > 0 && produtoSelecionado === carrinhoCompras[i].produto) {
-            console.log('é igual');
+            let itemRepetido = document.getElementById(carrinhoCompras[i].produto);
+
+            itemRepetido.remove();
+
             carrinhoCompras[i].unidades += quantidade
+            
+            console.log("carrinho", itemRepetido)
         } else {
             carrinhoCompras.push({
                 produto: produtoSelecionado,
@@ -64,14 +84,15 @@ function itensRepetidos(produtoSelecionado, quantidade) {
 
 function limpar() {
     let quantidade = parseInt(document.getElementById('quantidade').value = 1);
-    let listaDeProdutos = document.getElementById('carrinho-box');
     let textoFinal = document.createElement('span');
+    let textoTotal = document.querySelector('.carrinho__total').textContent; 
+
     textoFinal.id = 'texto-inicial-carrinho';
+    textoTotal.textContent = 'Total: R$0,00'
 
     if (carrinhoCompras.length > 0) {
         carrinhoCompras = [];
     }
-
 
     while (listaDeProdutos.firstChild) {
         listaDeProdutos.removeChild(listaDeProdutos.firstChild);
@@ -79,6 +100,4 @@ function limpar() {
 
     textoFinal.textContent = "Carrinho vazio";
     listaDeProdutos.appendChild(textoFinal);
-    console.log(listaDeProdutos);
-
 }
