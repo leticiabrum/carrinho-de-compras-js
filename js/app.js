70bca07fcd0d76ed2e9cdebfbbcd52cc77d0faf9
leticiabrum <leticiabrumcorrealeite@gmail.com>
2024-07-26 11:43:19 -0300

    create-app2.js(rascunho)

--- js/app.js
+++ js/app.js
@@ -2,19 +2,25 @@ let carrinhoCompras = [];
 let produtoSelecionado = document.getElementById('produto').value;
 let listaDeProdutos = document.getElementById('carrinho-box');
 let itemCarrinho = document.createElement('div');
+let textoTotal = document.querySelector('.carrinho__total').textContent;
+
 
 function adicionar() {
     let textoInicial = document.getElementById('texto-inicial-carrinho');
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let produtoSelecionado = document.getElementById('produto').value;
-    // let valorProduto = document.getElementsById('valor-produto').textContent;
+    let nomeProduto = produtoSelecionado.split('-')[0];
+    let valorProduto = parseInt(produtoSelecionado.split('$')[1]);
+    let valor = quantidade * valorProduto;
 
+
+    // console.log(textoTotal);
 
     if (carrinhoCompras.length == 0) {
         carrinhoCompras.push({
             produto: produtoSelecionado,
             unidades: quantidade,
-            // valor: valorProduto,
+            preco: valor,
         })
     } else {
         itensRepetidos(produtoSelecionado, quantidade);
@@ -24,40 +30,38 @@ function adicionar() {
         textoInicial.remove();
     }
 
-    // valorProduto.classList.add('texto-azul');
-
     itensCarrinho();
-
 }
 
 
 function itensCarrinho() {
     let sectionCarrinho = document.getElementById('carrinho-box');
-    // let valorProduto = document.getElementsByClassName('opcoes').textContent;
     let itemCarrinho = document.createElement('div');
-    let itemProduto = document.createElement('span');
     let itemUnidade = document.createElement('span');
-    // let itemValor = document.createElement('span');
-    
-    
+    let itemProduto = document.createElement('span');
+    let itemValor = document.createElement('span');
+
+    let textoAzul = itemValor.classList.add('texto-azul');
     itemUnidade.classList.add('texto-azul');
-    itemCarrinho.classList.add('produto-carrinho')
+    itemCarrinho.classList.add('produto-carrinho');
     itemCarrinho.id = 'item-carrinho';
-    
-    
-    
+
+
+
     for (let i = 0; i < carrinhoCompras.length; i++) {
         itemUnidade.id = carrinhoCompras[i].produto;
         itemProduto.id = carrinhoCompras[i].produto;
-        // itemValor.textContent = valorProduto;
-        itemProduto.textContent = carrinhoCompras[i].produto;
         itemUnidade.textContent = carrinhoCompras[i].unidades + "x ";
+        itemProduto.textContent = carrinhoCompras[i].produto;
+        itemValor.textContent = carrinhoCompras[i].preco;
         itemCarrinho.appendChild(itemUnidade);
         itemCarrinho.appendChild(itemProduto);
     }
 
+    // console.log('itemValor', itemValor.textContent);
+    // console.log(textoAzul);
+
     sectionCarrinho.appendChild(itemCarrinho);
-    // console.log('esse é o valor', valorProduto);
 }
 
 
@@ -67,11 +71,11 @@ function itensRepetidos(produtoSelecionado, quantidade) {
         if (carrinhoCompras.length > 0 && produtoSelecionado === carrinhoCompras[i].produto) {
             let itemRepetido = document.getElementById(carrinhoCompras[i].produto);
 
-            itemRepetido.remove();
+            // itemRepetido.remove();
 
             carrinhoCompras[i].unidades += quantidade
-            
-            console.log("carrinho", itemRepetido)
+
+            console.log('carrinho', itemRepetido)
         } else {
             carrinhoCompras.push({
                 produto: produtoSelecionado,
@@ -85,7 +89,6 @@ function itensRepetidos(produtoSelecionado, quantidade) {
 function limpar() {
     let quantidade = parseInt(document.getElementById('quantidade').value = 1);
     let textoFinal = document.createElement('span');
-    let textoTotal = document.querySelector('.carrinho__total').textContent; 
 
     textoFinal.id = 'texto-inicial-carrinho';
     textoTotal.textContent = 'Total: R$0,00'
@@ -98,6 +101,6 @@ function limpar() {
         listaDeProdutos.removeChild(listaDeProdutos.firstChild);
     }
 
-    textoFinal.textContent = "Carrinho vazio";
+    textoFinal.textContent = 'Carrinho vazio';
     listaDeProdutos.appendChild(textoFinal);
 }
---
+++ js/app2.js
@@ -0,0 +1,17 @@
+function adicionar() {
+//recuperar valores: nome do produto, quantidade e valor;
+let quantidade = document.getElementById('quantidade');
+let produto = document.getElementById('produto');
+let nomeProduto = produto.split('-')[0];
+let valorProduto = produto.split('$')[1];
+//adicionar os valores no carrinho;
+
+//calcular o subtotal;
+//calcular o totalFinal;
+
+
+}
+
+function limpar() {
+
+}
